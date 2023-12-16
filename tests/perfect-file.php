e593f9b0fb181c62da778059ba5eaaafd2a74264
William Desportes <williamdes@wdes.fr>
2023-12-16 22:44:28 +0100

    Add a perfect file for testing

---
+++ tests/perfect-file.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Wdes\Test;
+
+final class TheFinalClass
+{
+    public const TEST = true;
+
+    private function keepSecrets(): void
+    {
+    }
+
+    public function vault(): void
+    {
+    }
+
+}
